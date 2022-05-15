933a8d448ca8f9343a70e1fcceafae8567e1050c
nvshossj1 <nasho8866@gmail.com>
2022-05-14 20:21:07 -0400

    Agregar archivos de proyecto.

---
+++ EjercicioMensajero/Program.cs
@@ -0,0 +1,87 @@
+﻿using EjercicioMensajero.Comunicacion;
+using MensajeroModel;
+using MensajeroModel.DAL;
+using ServidorSocketUtils;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EjercicioMensajero
+{
+    class Program
+    {
+        private static IMensajesDAL mensajesDAL = ILecturaDAL.GetInstancia();
+        
+        static bool Menu()
+        {
+            bool continuar = true;
+            Console.WriteLine("Que desea hacer?");
+            Console.WriteLine("1. Ingresar \n2. Mostrar \n0. Salir");
+            switch (Console.ReadLine().Trim())
+            {
+                case "1": Ingresar(); 
+                    break;
+                case "2": Mostrar();
+                    break;
+                case "0": continuar = false;
+                    break;
+                default: Console.WriteLine("Ingrese de nuevo");
+                    break;
+            }
+            return continuar;
+        }
+
+        static void Main(string[] args)
+        {
+            //1. Iniciar el servidor socket en el puerto 3000
+            //2. El puerto tiene que ser configurable App.config
+            //3. Cuando reciba un cliente, tiene que solicitar a ese cliente el nombre
+            HebraServidor hebra = new HebraServidor();
+            Thread t = new Thread(new ThreadStart(hebra.Ejecutar));
+            t.Start();
+            //1. Como atender mas de un cliente a la vez?
+            //2. como evito que dos clientes ingresen a un archivo a la vez?
+            //3. como evitar el bloqueo mutuo? 
+            while (Menu()) ;
+        }
+
+        static void Ingresar()
+        {
+            Console.WriteLine("Ingrese medidor: ");
+            string medidor = Console.ReadLine().Trim();
+            Console.WriteLine("Ingrese Fecha: ");
+            string fecha = Console.ReadLine().Trim();
+            Console.WriteLine("Ingrese lectura: ");
+            string lectura = Console.ReadLine().Trim();
+            Mensaje mensaje = new Mensaje()
+            {
+                Medidor = medidor,
+                Fecha = fecha,
+                Lectura = lectura
+            };
+            //nuestro proyecto es ThreadSafe que significa que nos hacemos cargo de la concurrencia
+            lock (mensajesDAL)
+            { 
+                mensajesDAL.IngresarLectura(mensaje);
+            }
+        }
+
+        static void Mostrar()
+        {
+            List<Mensaje> mensajes = null;
+            lock (mensajesDAL)
+            {
+                mensajes = mensajesDAL.ObtenerLecturas();
+            }
+            foreach (Mensaje mensaje in mensajes)
+            {
+                Console.WriteLine(mensaje);
+            }
+        }
+    }
+}
---
+++ MensajeModelUtils/DAL/IMedidorDAL.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MensajeroModel.DAL
+{
+    public class IMedidorDAL : IMensajesDAL
+    {
+
+        //  para implementar un patron singleton
+        //  Singleton es un patrón de diseño del tipo creacional cuyo propósito es garantizar la
+        //  existencia de una sola instancia de una clase.Además el acceso a esa única instancia tiene que ser global.
+
+        //1. El contructor tiene que ser private
+        private IMedidorDAL()
+        {
+
+        }
+        //2. Debe poseer un atributo del mismo tipo de la clase y estatico
+        private static IMedidorDAL instancia;
+        //3. Tener un metodo GetIntance, que devuelve una referencia al atributo
+        public static IMensajesDAL GetInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new IMedidorDAL();
+            }
+            return instancia;
+        }
+
+        private static string url = Directory.GetCurrentDirectory();
+
+        private static string archivo = url + "/Lecturas.txt";
+        public void IngresarLectura(Mensaje mensaje)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(archivo, true))
+                {
+                    writer.WriteLine(mensaje.Medidor + "|" + mensaje.Fecha + "|" + mensaje.Lectura);
+                    writer.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        public List<Mensaje> ObtenerLecturas()
+        {
+            List<Mensaje> lista = new List<Mensaje>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(archivo))
+                {
+                    string texto = "";
+                    do
+                    {
+                        texto = reader.ReadLine();
+                        if (texto != null)
+                        {
+                            string[] arr = texto.Trim().Split(';');
+                            Mensaje mensaje = new Mensaje()
+                            {
+                                Medidor = arr[0],
+                                Fecha = arr[1],
+                                Lectura = arr[2]
+                            };
+                            lista.Add(mensaje);
+                        }
+                    } while (texto != null);
+                }
+            }
+            catch (Exception ex)
+            {
+                lista = null;
+            }
+            return lista;
+        }
+    }
+}
